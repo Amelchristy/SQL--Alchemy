9a1d2e8ce534e279230bd8be60e803610ea8b12f
Amelchristy <Amel_christy@users.noreply.github.com>
2021-01-09 13:14:31 -0500

    Final Push

--- App.py
+++ App.py
@@ -1,25 +1,20 @@
 from flask import Flask, jsonify
-
-# Dependencies and Setup
 import numpy as np
 import datetime as dt
-
-# Python SQL Toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.pool import StaticPool
 
-#################################################
+
 # Database Setup
-#################################################
-# Reference: https://stackoverflow.com/questions/33055039/using-sqlalchemy-scoped-session-in-theading-thread
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
 
-# Reflect Existing Database Into a New Model
+
 Base = automap_base()
-# Reflect the Tables
+
 Base.prepare(engine, reflect=True)
 
 # Save References to Each Table
@@ -29,14 +24,13 @@ Station = Base.classes.station
 # Create Session (Link) From Python to the DB
 session = Session(engine)
 
-#################################################
 # Flask Setup
-#################################################
+
 app = Flask(__name__)
 
-#################################################
-# Flask Routes
-#################################################
+
+# Flask Routes:
+
 # Home Route
 @app.route("/")
 def welcome():
